d52f500ad92b313eec71566c7ffb222c9e64963b
InvertedCarrot <dyuan9378@gmail.com>
2025-05-11 17:16:14 -0400

    basic animation tree for player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,28 +1,21 @@
 extends CharacterBody2D
 
 @onready var player_animation = $PlayerAnimation
-var pointing_direction = 0
+var speed = 300
+var direction: Vector2
+
 
 func _ready() -> void:
-	position = Vector2(50,50)
+	position = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	
 	# directional input
-	var direction: Vector2 = Input.get_vector("left","right","up","down")
-	velocity = direction * 500
-	if velocity.x!=0:
-		player_animation.play("new_animation")
-		player_animation.advance(0)
-	else:
-		player_animation.stop()
-	
-	# -1 for left, 1 for right, 0 otherwise
-	pointing_direction = Input.get_axis("left","right")
-	if pointing_direction<0:
-		$PlayerImage.flip_h = true
-	else:
-		$PlayerImage.flip_h = false
-	
+	direction = Input.get_vector("left","right","up","down") # unit vector
+	velocity = direction * speed
+
+
+
+func _physics_process(delta: float) -> void:
 	move_and_slide()
---
+++ scenes/player/player_animation_tree.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+@export var animation_tree: AnimationTree
+@onready var player = get_owner()
+
+var last_facing_direction: Vector2
+
+func _physics_process(delta: float) -> void:
+	
+	var is_idle = (player.direction == Vector2.ZERO)
+	# if moving, update the player's direction
+	# else, keep track of the player's last direction since standing still
+	if !is_idle:
+		last_facing_direction = player.direction
+	#
+	#animation_tree.set("parameters/conditions/idle", is_idle)
+	#animation_tree.set("parameters/conditions/run", !is_idle)
+	
+	# set blend positions wrt player's direction
+	animation_tree.set("parameters/Run/blend_position", last_facing_direction)
+	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
