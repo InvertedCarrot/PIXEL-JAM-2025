ebd6fa21058baac5e49230b255aa8f5183ac9df1
asterbot <arjunsodhi7@gmail.com>
2025-05-13 17:35:55 -0400

    reaper chases player, not fireball

--- scenes/entities/fireball/fireball.gd
+++ scenes/entities/fireball/fireball.gd
@@ -2,11 +2,12 @@ extends Entity
 
 func _ready() -> void:
 	can_control = false
-	start_position = Vector2(500,500)
+	start_position = Vector2(200,200)
 	super()
 
 func _process(delta: float) -> void:
-	direction = ($"../Cat".position - position).normalized()
-	velocity = direction * speed
-	move_and_slide()
+	#does not go to player yet
+	#direction = ($"../Cat".position - position).normalized()
+	#velocity = direction * speed
+	#move_and_slide()
 	super(delta)
